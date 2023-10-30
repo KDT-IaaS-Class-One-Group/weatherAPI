9f14aeb96be0f0c51bb8a128dd553dcbfdefff0f
YoonJunHyeon <qmdldos@gmail.com>
2023-10-30 18:45:29 +0900

    response 변수 선언

--- public/module/daejeon.js
+++ public/module/daejeon.js
@@ -4,9 +4,11 @@ const deajeonFetch = async () => {
   // 필요한 변수 설정
   const apiKey = 'af424409722cecb0056cea43492fb33c'; // 본인의 API 키로 대체
   const city = 'daejeon'; // 원하는 도시명으로 변경 가능
+
   // API 요청
-  await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-    try {
+  try {
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+      
 
     }
     catch(error) {
