c1f76a42be12427776adb00c6992ee5dfe31409c
kimhyun <forhn37@gmail.com>
2023-10-30 18:46:19 +0900

    import, export를 통한 모듈 호출시문제 발생

--- public/module/Dom.js
+++ public/module/Dom.js
@@ -1,16 +1,23 @@
-const seoul = require('./seoul')
-const jejudo = require('./jejudo')
-const daejeon = require("./daejeon")
-
-
+import { getWeatherData } from './seoul.js'
+getWeatherData() 
+    .then(data => {
+        console.log(`서울 온도: ${data.seoulTemp}°C`);
+        console.log(`서울 습도: ${data.seoulHumi}%`);
+    })
+    .catch(error => {
+        console.error("날씨 정보를 가져오는데 실패했습니다.", error);
+    });
+    
 
 // 서울 온도, 습도 설정
-document.getElementById('seoul-temperature').textContent = `${seoul.seoulTemp}°C`;
-document.getElementById('seoul-humidity').textContent = `${seoul.seoulHumi}%`;
-// 대전 온도, 습도 설정
-document.getElementById('daejeon-temperature').textContent = `${daejeon.seoulTemp}°C`;
-document.getElementById('daejeon-humidity').textContent = `${daejeon.seoulHumi}%`;
-// 제주도 온도, 습도 설정s
-document.getElementById('jeju-temperature').textContent = `${jejudo.seoulTemp}°C`;
-document.getElementById('jeju-humidity').textContent = `${jejudo.seoulHumi}%`;
+// document.getElementById('seoul-temperature').textContent 
+
+// document.getElementById('seoul-humidity').textContent = `${seoul.seoulHumi}%`;
+//   // 대전 온도, 습도 설정
+// document.getElementById('daejeon-temperature').textContent = `${daejeon.seoulTemp}°C`;
+// document.getElementById('daejeon-humidity').textContent = `${daejeon.seoulHumi}%`;
+// // 제주도 온도, 습도 설정s
+// document.getElementById('jeju-temperature').textContent = `${jejudo.seoulTemp}°C`;
+// document.getElementById('jeju-humidity').textContent = `${jejudo.seoulHumi}%`;
+
 
