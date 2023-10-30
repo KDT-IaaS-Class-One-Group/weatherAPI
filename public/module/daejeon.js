719c6c10c5959cd8654b012414a483605f3938e3
YoonJunHyeon <qmdldos@gmail.com>
2023-10-30 18:44:15 +0900

    deajeon 구조 갈아 엎는 중, 함수화 할 땐, then을 본문에서 쓰도록 fetch 구문만 남겨놓는 방안을 채택한다.

--- public/module/daejeon.js
+++ public/module/daejeon.js
@@ -1,16 +1,18 @@
-// 필요한 변수 설정
-const apiKey = 'af424409722cecb0056cea43492fb33c'; // 본인의 API 키로 대체
-const city = 'Seoul'; // 원하는 도시명으로 변경 가능
-// API 요청
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-    .then(response => response.json())
-    .then(data => {
-        const temperature = data.main.temp; // 온도
-        const humidity = data.main.humidity; // 습도
-        console.log(`도시: ${city}`);
-        console.log(`온도: ${temperature}°C`);
-        console.log(`습도: ${humidity}%`);
-    })
-    .catch(error => {
-        console.error("날씨 정보를 가져오는데 실패했습니다.", error);
-    });
+// console.log('hi');
+
+const deajeonFetch = async () => {
+  // 필요한 변수 설정
+  const apiKey = 'af424409722cecb0056cea43492fb33c'; // 본인의 API 키로 대체
+  const city = 'daejeon'; // 원하는 도시명으로 변경 가능
+  // API 요청
+  await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+    try {
+
+    }
+    catch(error) {
+      console.error("날씨 정보 가져올 수 없습니다.");
+    }
+}
+
+deajeonFetch().then( result => console.log(result));
+
