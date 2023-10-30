0dbb07e119b7dbd5241e45ade96ec5ff5e6b5deb
YoonJunHyeon <qmdldos@gmail.com>
2023-10-30 19:40:18 +0900

    dom api 수정 완료

--- public/module/Dom.js
+++ public/module/Dom.js
@@ -9,7 +9,7 @@ getWeatherSeoul()
   // 서울 온도, 습도 설정
   const seoulTemperatureElement = document.getElementById("seoul-temperature");
   seoulTemperatureElement.textContent = data.seoulTemp;
-  document.getElementById("seoul-humidity").textContent = `${seoul.seoulHumi}%`;
+  document.getElementById("seoul-humidity").textContent = `${data.seoulHumi}%`;
  })
  .catch((error) => {
   console.error("서울 날씨 정보를 가져오는데 실패했습니다.", error);
@@ -22,8 +22,8 @@ getWeatherJeju()
   // 제주도 온도, 습도 설정s
   document.getElementById(
    "jeju-temperature"
-  ).textContent = `${jejudo.seoulTemp}°C`;
-  document.getElementById("jeju-humidity").textContent = `${jejudo.seoulHumi}%`;
+  ).textContent = `${data.jejuTemp}°C`;
+  document.getElementById("jeju-humidity").textContent = `${data.jejuHumi}%`;
  })
  .catch((error) => {
   console.error("제주 날씨 정보를 가져오는데 실패했습니다.", error);
@@ -35,10 +35,10 @@ getWeatherDaejeon()
   // 대전 온도, 습도 설정
   document.getElementById(
    "daejeon-temperature"
-  ).textContent = `${daejeon.seoulTemp}°C`;
+  ).textContent = `${data.daejeonTemp}°C`;
   document.getElementById(
    "daejeon-humidity"
-  ).textContent = `${daejeon.seoulHumi}%`;
+  ).textContent = `${data.daejeonHumi}%`;
  })
  .catch((error) => {
   console.error("대전 날씨 정보를 가져오는데 실패했습니다.", error);
