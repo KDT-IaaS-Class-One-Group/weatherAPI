893cb24914377a253edca935f9eb8322bcc6361c
HongMoonGI <hmoongi94@gmail.com>
2023-11-01 16:57:21 +0900

    온도가 이상하게 나옴

--- public/module/Dom.js
+++ public/module/Dom.js
@@ -1,45 +1,17 @@
-import { getWeatherSeoul } from "./seoul.js";
-import { getWeatherJeju } from "./jejudo.js";
-import { getWeatherDaejeon } from "./daejeon.js";
+// Dom.js
+import { fetchWeatherData } from "./city.js";
 
-getWeatherSeoul()
- .then((data) => {
-  console.log(`서울 온도: ${data.seoulTemp}°C`);
-  console.log(`서울 습도: ${data.seoulHumi}%`);
-  // 서울 온도, 습도 설정
-  const seoulTemperatureElement = document.getElementById("seoul-temperature");
-  seoulTemperatureElement.textContent = data.seoulTemp;
-  document.getElementById("seoul-humidity").textContent = `${data.seoulHumi}%`;
- })
- .catch((error) => {
-  console.error("서울 날씨 정보를 가져오는데 실패했습니다.", error);
- });
+document.getElementById('city-form').addEventListener('submit', function (submit) {
+  submit.preventDefault(); // 폼 제출 기본 동작 막기
+  const cityInput = document.getElementById('city-input');
+  document.getElementById('cityname').textContent = cityInput.value; // 도시 이름 가져오기
 
-getWeatherJeju()
- .then((data) => {
-  console.log(`제주 온도: ${data.jejuTemp}°C`);
-  console.log(`제주 습도: ${data.jejuHumi}%`);
-  // 제주도 온도, 습도 설정s
-  document.getElementById(
-   "jeju-temperature"
-  ).textContent = `${data.jejuTemp}°C`;
-  document.getElementById("jeju-humidity").textContent = `${data.jejuHumi}%`;
- })
- .catch((error) => {
-  console.error("제주 날씨 정보를 가져오는데 실패했습니다.", error);
- });
-getWeatherDaejeon()
- .then((data) => {
-  console.log(`대전 온도: ${data.daejeonTemp}°C`);
-  console.log(`대전 습도: ${data.daejeonHumi}%`);
-  // 대전 온도, 습도 설정
-  document.getElementById(
-   "daejeon-temperature"
-  ).textContent = `${data.daejeonTemp}°C`;
-  document.getElementById(
-   "daejeon-humidity"
-  ).textContent = `${data.daejeonHumi}%`;
- })
- .catch((error) => {
-  console.error("대전 날씨 정보를 가져오는데 실패했습니다.", error);
- });
+  fetchWeatherData(cityInput.value)
+    .then((data) => {
+      document.getElementById('city-temperature').textContent = data.cityTemp;
+      document.getElementById('city-humidity').textContent = data.cityHumi;
+    })
+    .catch((error) => {
+      console.error("데이터를 가져오는데 실패했습니다.", error);
+    });
+});
---
+++ public/module/city.js
@@ -0,0 +1,15 @@
+// city.js
+export async function fetchWeatherData(inputValue) {
+  try {
+    const apikey = '13e787bc80632c2e192c0e6abff23bee';
+    const city = inputValue; // 원하는 도시 이름으로 변경
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`);
+    const data = await response.json();
+    const cityTemp = data.main.temp;
+    const cityHumi = data.main.humidity;
+    return { cityTemp, cityHumi };
+  } catch (error) {
+    console.error("데이터를 가져오는데 실패했습니다.", error);
+    throw error; // 에러를 다시 던져서 .catch()에서 처리할 수 있도록
+  }
+}
