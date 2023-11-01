4b2ebf059c54d37a51ebb16e0978f243e0a872b7
HongMoonGI <hmoongi94@gmail.com>
2023-11-01 17:13:23 +0900

    켈빈 정리

--- public/module/city.js
+++ public/module/city.js
@@ -1,5 +1,5 @@
 // city.js
-export async function fetchWeatherData(inputValue) {
+async function fetchWeatherData(inputValue) {
   try {
     const apikey = '13e787bc80632c2e192c0e6abff23bee';
     const city = inputValue; // 원하는 도시 이름으로 변경
@@ -7,9 +7,11 @@ export async function fetchWeatherData(inputValue) {
     const data = await response.json();
     const cityTemp = data.main.temp;
     const cityHumi = data.main.humidity;
+    console.log(data)
     return { cityTemp, cityHumi };
   } catch (error) {
     console.error("데이터를 가져오는데 실패했습니다.", error);
     throw error; // 에러를 다시 던져서 .catch()에서 처리할 수 있도록
   }
 }
+fetchWeatherData("Daejeon")
