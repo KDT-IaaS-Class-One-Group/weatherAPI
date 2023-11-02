9136435f1b5808f70ce870d45feef66457b31907
YoonJunHyeon <qmdldos@gmail.com>
2023-11-02 11:24:17 +0900

    사용자 측면 경험을 높이는 방향으로 간단한 추가로 가성비 높은 효과를 노린다. 구조는 우리가 생각했던 그대로이기 때문에 좋았다.

--- public/module/city.js
+++ public/module/city.js
@@ -5,7 +5,9 @@ export async function fetchWeatherData(inputValue) {
     const city = inputValue; // 원하는 도시 이름으로 변경
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`);
     const data = await response.json();
-    const cityTemp = data.main.temp;
+    const cityTemp = parseInt(data.main.temp - 273.15);
+    // * 소수점을 지우는 메서드와 절대값을 지우는 간단한 처리로
+    // *  가성비 높게 사용감을 높일 수 있다.
     const cityHumi = data.main.humidity;
     console.log(data)
     return { cityTemp, cityHumi };
